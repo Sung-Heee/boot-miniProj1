dffddfa586c476a60458839ac6fbdae6ffe18720
Sung-Heee <praaa0220@gmail.com>
2024-04-09 17:40:41 +0900

    Initial project setup

---
+++ src/main/java/com/example/mini/BootMiniProj1Application.java
@@ -0,0 +1,13 @@
+package com.example.mini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMiniProj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMiniProj1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mini/BootMiniProj1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootMiniProj1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
